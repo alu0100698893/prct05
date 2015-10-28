709ef1440369ac99965e27ba717f97bfbaefe8cd
Alberto <alu0100698893@ull.edu.es>
2015-10-28 09:16:08 +0000

    pruebas unitarias completas version final

--- test/tc_racionales.rb
+++ test/tc_racionales.rb
@@ -8,7 +8,7 @@ class Test_Fraciones < Test::Unit::TestCase
 	def setup #metodo que trae la clase Test::Unit el cual debe realizarse 
 		  #despues o antes de cada prueba es como un constructor de objetos
 	   @racional1 = Fracciones.new(8,30) 
-	   @racional2 = Fracciones.new(-6,12)
+	   @racional2 = Fracciones.new(6,12)
 	end
 
 	def teardown #trabaja de manera paracida al setup
@@ -23,38 +23,42 @@ class Test_Fraciones < Test::Unit::TestCase
 	end
 
 	def test_suma #para realizar solo las pruebas con la operacion suma
-           rac1 = @racional1
- 	   rac2 = @racional2
-           rac3 = rac1 + rac2
-           puts "El resultado de la operacion es: #{rac1} + #{rac2} = #{rac3}"
+           #rac1 = @racional1
+ 	   #rac2 = @racional2
+           #rac3 = rac1 + rac2
+           #puts "El resultado de la operacion es: #{rac1} + #{rac2} = #{rac3}"
+	   assert_equal("7/30",(@racional1 + @racional2).to_s)
 	end
 
 	def test_resta #para realizar solo las pruebas con la operacion resta
-	   rac1 = @racional1
- 	   rac2 = @racional2
-           rac3 = rac1 - rac2
-           puts "El resultado de la operacion es: #{rac1} - #{rac2} = #{rac3}"
+	   #rac1 = @racional1
+ 	   #rac2 = @racional2
+           #rac3 = rac1 - rac2
+           #puts "El resultado de la operacion es: #{rac1} - #{rac2} = #{rac3}"
+	   assert_equal("23/30",(@racional1 - @racional2).to_s)
 	end
 
 	def test_multiplicacion #para realizar solo las pruebas con la operacion de multiplicacion
-           rac1 = @racional1
- 	   rac2 = @racional2
-           rac3 = rac1 * rac2
-           puts "El resultado de la operacion es: #{rac1} * #{rac2} = #{rac3}"
+           #rac1 = @racional1
+ 	   #rac2 = @racional2
+           #rac3 = rac1 * rac2
+           #puts "El resultado de la operacion es: #{rac1} * #{rac2} = #{rac3}"ç
+	   assert_equal("1/10",(@racional1 * @racional2).to_s)
         end
 
 	def test_division #para realizar solo las pruebas con la operacion de division
-	   rac1 = @racional1
- 	   rac2 = @racional2
-           rac3 = rac1 / rac2
-           puts "El resultado de la operacion es: #{rac1} / #{rac2} = #{rac3}"
+	   #rac1 = @racional1
+ 	   #rac2 = @racional2
+           #rac3 = rac1 / rac2
+           #puts "El resultado de la operacion es: #{rac1} / #{rac2} = #{rac3}"
+	   assert_equal("8/15",(@racional1 / @racional2).to_s)
 	end
 
-	def test_type_check
-           assert_raise(RuntimeError) {Fracciones.new('6','12')}
-        end
+	#def test_type_check
+         #  assert_raise(RuntimeError) {Fracciones.new('6','12')} #RutimeError cuando se intenta realizar una operacion no valida
+        #end
 
 	def test_failure
-	   assert_equal("(8,30)",Fracciones.new(8,30).to_s)
+	   assert_equal("8/30",Fracciones.new(8,30).to_s)
         end
 end
